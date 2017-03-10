328c0b487ed470544bed39c8b079e1bab2c5693c
Jesse Lessard <jhlessard@live.com>
2017-03-09 18:11:06 -0800

    first file creation

---
+++ RocketSim.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2.7
+
+"""RocketSim.py: RocketSim main run file"""
+
+__author__ = "Jesse Lessard"
+__copyright__ = "Copyright 2017, Jesse Lessard"
+__license__ = "GPLv3"
+__maintainer__ = "Jesse Lessard"
+__email__ = "jhlessard@live.com"
+__status__ = "Development"
